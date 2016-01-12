ea291f88bb3c4feea038dc027bd4694022bce06b
Javier Gusano Martinez <jgm1986@hotmail.com>
2016-01-12 17:29:51 +0100

    New script comments.

--- dnsRequests.sh
+++ dnsRequests.sh
@@ -4,11 +4,17 @@ set -e
 # Get the current path.
 pwd_dir=$(pwd)
 
+# Create temporal directory
 $pwd_dir/include/newTmpDir.sh
 cd tmp
+
+# Download the Alexa list and execute DNS query.
 $pwd_dir/include/alexa.sh
 $pwd_dir/include/dnsRequests.sh $1
 $pwd_dir/include/rmAlexa.sh
+
+# Clean the temporal directory
 cd ..
 $pwd_dir/include/rmTmpDir.sh
 
+exit 0
