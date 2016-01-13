60f809ea239514190992fad4e88a1f1e19774a65
Javier Gusano Martinez <jgm1986@hotmail.com>
2016-01-13 11:46:45 +0100

    Nslookup has been replaced by dig command

--- include/dnsRequests.sh
+++ include/dnsRequests.sh
@@ -7,5 +7,8 @@ if [ -z "$1" ]
   then
     NUM=1
 fi
-head -n $NUM top-1m.csv | cut -f2 -d "," | nslookup
+
+#head -n $NUM top-1m.csv | cut -f2 -d "," | nslookup
+dig @80.58.0.33 $(head -n $1 top-1m.csv | cut -f2 -d ",")
+
 echo "Done!"
